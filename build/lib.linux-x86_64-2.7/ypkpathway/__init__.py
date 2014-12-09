69236ba1f463d1c6ef3548f350b1861d90e56e03
Björn Johansson <bjornjobb@gmail.com>
2014-12-09 11:42:20 -0800

    first commit

--- build/lib.linux-x86_64-2.7/ypkpathway/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__version__ = "0.0.9"
-__author__  = "Björn Johansson"
-__email__   = "bjorn_johansson@bio.uminho.pt"
-
-from ypkpathway import PathWay
-
--- build/lib/ypkpathway/data/pCAPs.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from pydna import Genbank
-gb = Genbank("bjornjobbb@gmail.com")
-pCAPs = gb.nucleotide("AJ001614")
-pCAPs.features = pCAPs.features [1:]
-
