06cce62b98fa4ff08c9b2edf231ab9555d99b8b1
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2015-07-23 02:17:47 +0100

    binstar2

--- ypkpathway/__init__.py
+++ ypkpathway/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__author__      ="Björn Johansson"
-__date__        ="April 17, 2015"
-__copyright__   = "Copyright 2013, 2014, 2015 Björn Johansson"
-__credits__     = ["Björn Johansson"]
-__license__     = "BSD"
-__maintainer__  = "Björn Johansson"
-__email__       = "bjorn_johansson@bio.uminho.pt"
+__author__      =u"Björn Johansson"
+__date__        =u"April 17, 2015"
+__copyright__   =u"Copyright 2013, 2014, 2015 Björn Johansson"
+__credits__     = [u"Björn Johansson"]
+__license__     = u"BSD"
+__maintainer__  = u"Björn Johansson"
+__email__       = u"bjorn_johansson@bio.uminho.pt"
 
 from ._version import get_versions
 __version__      = get_versions()['version'][:5]
