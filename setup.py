dc1e3a13393e250ed3140f944e6d05731a36cb3e
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2017-06-24 11:55:01 +0100

    fix

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import versioneer
-# Read author etc..
+
+# Read author etc. from __init__.py
 for line in open('ypkpathway/__init__.py'):
     if line.startswith('__') and not line.startswith('__version') and not line.startswith('__long'):
         exec(line.strip())
+
 from setuptools import setup
-import os#, codecs
+import os
+with open("README.md") as f:
+    long_description = f.read()
+    
+try:
+    from pypandoc import convert_file
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    with open("README.md", encoding="utf-8") as f:
+        long_description = f.read()
+else:
+    long_description = "\n"+convert_file("README.md", 'rst')   
 
 setup(  name='ypkpathway',
         version=versioneer.get_version()[:5],
@@ -20,10 +34,7 @@ setup(  name='ypkpathway',
         url='http://pypi.python.org/pypi/ypkpathway/',
         license='LICENSE.txt',
         description='''Simulation and documentation of metabolic pathway assembly using the Yeast Pathway Kit.''',
-        #long_description=codecs.open('README.rst', "r", "utf8").read(),
-        long_description=open('README.rst').read(),
-        #setup_requires=['setuptools-markdown'],
-        #long_description_markdown_filename='README.md',
+        long_description=long_description,
         install_requires =[ "pydna", "ipython", "docopt", "notedown"],
         test_suite="run_tests.load_my_tests",
         zip_safe = False,
